9213d332c48407d6e2e5303f9f752f72d62952e7
Mohammadreza Hendiani <mohammad.r.hendiani@gmail.com>
2024-01-16 01:33:47 +0330

    rewrite to make replacement of text easier

--- script/dl.sh
+++ script/dl.sh
@@ -1,3 +1,50 @@
 #!/bin/sh
-line0="OS:+Engineer+Gen+Z+(Mohammadreza+Hendiani)+noarch"
-curl -go ~/man2dev/img/fetchme.svg "https://readme-typing-svg.demolab.com/?random=false&height=585&width=1200&size=20&font=monospace&color=11D116&center=false&vCenter=true&multiline=true&duration=60&pause=50&repeat=flase&separator=;&lines=~%3E%20fetchme;          _____                    _____          +man@dev;         /\    \                  /\    \         +-------;        /::\____\                /::\____\        +$line0;       /::::|   |               /:::/    /        +Host:+Carbon-based+lifeform;      /:::::|   |              /:::/    /         +Kernel:+Consciousness-∞-000.hZ.noarch;     /::::::|   |             /:::/    /          +Uptime:+25+years,+1+day,+12+hours,+29+mins;    /:::/|::|   |            /:::/____/           +Packages:+∞;   /:::/ |::|   |           /::::\    \           +Shell:+The+Internet;  /:::/  |::|___|______    /::::::\    \   _____  +Resolution:+Native+3D; /:::/   |::::::::\    \  /:::/\:::\    \ /\    \ +DE:+Mohammadreza+Hendiani+25.1.10;/:::/    |:::::::::\____\/:::/  \:::\    /::\____\+WM:+Gravity;\::/    / ~~~~~/:::/    /\::/    \:::\  /:::/    /+Theme:+Reality; \/____/      /:::/    /  \/____/ \:::\/:::/    / +Icons:+corporeal;             /:::/    /            \::::::/    /  +Terminal:+English+(CEFR+C1)+7.0.8;            /:::/    /              \::::/    /   +Terminal+Font:+IELTS+UKVI+Academic+(7.0);           /:::/    /               /:::/    /    +CPU:+Bachelor+of+Computer+Engineering;          /:::/    /               /:::/    /     +GPU+1:+Persistent;         /:::/    /               /:::/    /      +GPU+2:+Passion;        /:::/    /               /:::/    /       +Memory:+∞+/+∞;        \::/    /                \::/    /        +Disk+(/):+∞+/+∞+-+btrfs;         \/____/                  \/____/         +Battery:+100%+[Full];                                                  +Locale:+en_US.UTF-8;"
+
+ascii00='          _____                    _____          '
+ascii01='         /\    \                  /\    \         '
+ascii02='        /::\____\                /::\____\        '
+ascii03='       /::::|   |               /:::/    /        '
+ascii04='      /:::::|   |              /:::/    /         '
+ascii05='     /::::::|   |             /:::/    /          '
+ascii06='    /:::/|::|   |            /:::/____/           '
+ascii07='   /:::/ |::|   |           /::::\    \           '
+ascii08='  /:::/  |::|___|______    /::::::\    \   _____  '
+ascii09=' /:::/   |::::::::\    \  /:::/\:::\    \ /\    \ '
+ascii10='/:::/    |:::::::::\____\/:::/  \:::\    /::\____\'
+ascii11='\::/    / ~~~~~/:::/    /\::/    \:::\  /:::/    /'
+ascii12=' \/____/      /:::/    /  \/____/ \:::\/:::/    / '
+ascii13='             /:::/    /            \::::::/    /  '
+ascii14='            /:::/    /              \::::/    /   '
+ascii15='           /:::/    /               /:::/    /    '
+ascii16='          /:::/    /               /:::/    /     '
+ascii17='         /:::/    /               /:::/    /      '
+ascii18='        /:::/    /               /:::/    /       '
+ascii19='        \::/    /                \::/    /        '
+ascii20='         \/____/                  \/____/         '
+ascii21='                                                  '
+
+host="man@dev"
+line="-------"
+
+line00="OS:+Engineer+Gen+Z+(Mohammadreza+Hendiani)+noarch"
+line01="Host:+Carbon-based+lifeform"
+line02="Kernel:+Consciousness-∞-000.hZ.noarch"
+line03="Uptime:+25+years,+1+day,+12+hours,+29+mins"
+line04="Packages:+∞"
+line05="Shell:+The+Internet"
+line06="Resolution:+Native+3D"
+line07="DE:+Mohammadreza+Hendiani+25.1.10"
+line08="WM:+Gravity"
+line09="Theme:+Reality"
+line10="Icons:+corporeal"
+line11="Terminal:+English+(CEFR+C1)+7.0.8"
+line12="Terminal+Font:+IELTS+UKVI+Academic+(7.0)"
+line13="CPU:+Bachelor+of+Computer+Engineering"
+line14="GPU+1:+Persistent"
+line15="GPU+2:+Passion"
+line16="Memory:+∞+/+∞"
+line17="Disk+(/):+∞+/+∞+-+btrfs"
+line18="Battery:+100%+[Full]"
+line19="Locale:+en_US.UTF-8"
+
+curl -go ~/man2dev/img/fetchme.svg "https://readme-typing-svg.demolab.com/?random=false&height=585&width=1200&size=20&font=monospace&color=11D116&center=false&vCenter=true&multiline=true&duration=60&pause=50&repeat=flase&separator=;&lines=~%3E%20fetchme;$ascii00+$host;$ascii01+$line;$ascii02+$line00;$ascii03+$line01;$ascii04+$line02;$ascii05+$line03;$ascii06+$line04;$ascii07+$line05;$ascii08+$line06;$ascii09+$line07;$ascii10+$line08;$ascii11+$line09;$ascii12+$line10;$ascii13+$line11;$ascii14+$line12;$ascii15+$line13;$ascii16+$line14;$ascii17+$line15;$ascii18+$line16;$ascii19+$line17;$ascii20+$line18;$ascii21+$line19;"
